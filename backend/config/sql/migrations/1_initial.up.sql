17bb5d9db31ebe223ac619a2dac8a511aaf8d8dc
Antoine Leblanc <antoine@imparfaiteparis.com>
2020-07-10 23:06:52 +0200

    Reset

--- backend/config/sql/migrations/1_initial.up.sql
+++
@@ -1,9 +0,0 @@
--- +migrate Up
-CREATE TABLE articles
-(
-    id      uuid PRIMARY KEY,
-    title   varchar(200) NOT NULL,
-    url     varchar(255) NOT NULL,
-    savedon timestamp    not null,
-    readon  timestamp
-);
--- backend/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/grepsd/knowledge-database
-
-go 1.14
-
-require (
-	github.com/golang-migrate/migrate/v4 v4.11.0
-	github.com/google/uuid v1.1.1
-	github.com/lib/pq v1.7.0
-)
